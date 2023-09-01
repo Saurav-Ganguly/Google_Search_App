67501f396ca11b60ec5502c4541ad7e73329c626
Saurav Ganguly <hisauravganguly@gmail.com>
2023-09-01 08:37:08 +0530

    added search page UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_clone/color.dart';
 import 'package:google_clone/responsive/mobile_screen_layout.dart';
 import 'package:google_clone/responsive/responsive_layout_screen.dart';
 import 'package:google_clone/responsive/web_screen_layout.dart';
+import 'package:google_clone/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,10 +22,11 @@ class MyApp extends StatelessWidget {
         //get all colors from the color.dart file
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const ResponsiveLayoutScreen(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      // home: const ResponsiveLayoutScreen(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: const SearchScreen(),
     );
   }
 }
---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_clone/color.dart';
+import 'package:google_clone/widgets/search_footer.dart';
+import 'package:google_clone/widgets/search_header.dart';
+import 'package:google_clone/widgets/search_tabs.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Web Header
+            const SearchHeader(),
+            const Padding(
+              padding: EdgeInsets.only(left: 150.0),
+              child: SearchTabs(),
+            ),
+            const Divider(
+              height: 0,
+              thickness: 0.3,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      '< Prev',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: blueColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Next >',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: blueColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              width: 30,
+            ),
+            const SearchFooter(),
+          ],
+        ),
+      ),
+    );
+  }
+}
